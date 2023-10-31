69cac464e574000815879387c8ec80bd00603f02
platara <plat0033@algonquinlive.com>
2023-10-31 18:44:13 -0400

    Add wk9 starter files

---
+++ week-09/starter/main.js
@@ -0,0 +1,19 @@
+/*** Update form ***/
+// 1.Create a button in the student card(script file), and retrieve it(main js file). Use the bs modal.
+
+// 2.Add an event listener to the button (optional).
+
+// 3.Insert a 'HTML form' in the bs modal.
+
+// 4.Add an event listener to the 'form'. Update the student info.
+
+/*** Filter ***/
+// 5. Add an event listener to the 'Search' form. Use the 'submit' event. Display a filtered list based on the student name.
+
+// 6.Add an event listener to the 'Search' element. Use the 'input' event. Display a filtered list based on the student name.
+
+
+
+
+
+
